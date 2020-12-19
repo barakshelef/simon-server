26bfc53c4a0fc8f882e84ab8a803cef554371352
shelef <shelef@guardicore.com>
2020-12-19 18:37:08 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Simon Relay Server
 The Simon Relay Server. It relays.
+## Protocol
+### Register Client
+To register your client connect to the server and send the Game ID:
+```python
+import socket
+
+SERVER_ADDRESS = ("localhost", 9999)
+
+s = socket.socket()
+s.connect(SERVER_ADDRESS)
+
+GAME_ID = 12345
+s.send(bytes(str(GAME_ID), "ascii"))
+
+if s.recv(1024) == b"OK":
+    print("Connected!")
+```
+If you receive the `OK` message then registration was successful.
+
+### Send and receive data
+Once you have a registered client, any data you send will be relayed to all other clients registered to your Game ID.
+And you shall receive every message sent by all other clients registered to your Game ID.
+
+### End Game
+Simply send the `END_GAME` message over your registered client socket.
+
+### Stop Server
+Create a new client and send the `STOP_SERVER` message when registering, stead of Game ID.
---
+++ main.py
@@ -0,0 +1,100 @@
+import socket
+import threading
+import socketserver
+from typing import Dict
+
+HANDSHAKE_OK_MESSAGE = "OK"
+STOP_SERVER_MESSAGE = "STOP_SERVER"
+END_GAME_MESSAGE = "END_GAME"
+SERVER_STOP_EVENT = threading.Event()
+
+
+class Game:
+    def __init__(self, game_id: int):
+        self.id = game_id
+        self._requests = []
+        self._event = threading.Event()
+
+    def register(self, request: socket.socket):
+        player_id = len(self._requests)
+        self._requests.append(request)
+        return player_id
+
+    def sendall(self, data: bytes):
+        if not self._requests:
+            return
+
+        for request in self._requests:
+            request.sendall(data)
+
+    def has_ended(self):
+        return self._event.is_set()
+
+    def end(self):
+        return self._event.set()
+
+
+g_new_storage: Dict[int, Game] = {}
+
+
+class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
+    def setup(self):
+        handshake_message = str(self.request.recv(1024), 'ascii')
+        if handshake_message == STOP_SERVER_MESSAGE:
+            print("Got STOP_SERVER_MESSAGE handshake")
+            SERVER_STOP_EVENT.set()
+            return
+
+        game_id = int(handshake_message)
+        if game_id not in g_new_storage:
+            g_new_storage[game_id] = Game(game_id)
+
+        self._game = g_new_storage[game_id]
+        self._player_id = self._game.register(self.request)
+
+        print(f"Registered game ID: {self._game.id} player #{self._player_id}")
+        self.request.sendall(bytes(HANDSHAKE_OK_MESSAGE, 'ascii'))
+
+        self.request.settimeout(1)
+
+    def handle(self):
+        while not SERVER_STOP_EVENT.is_set() and not self._game.has_ended():
+            try:
+                data = self.request.recv(1024)
+            except socket.timeout:
+                continue
+
+            message = str(data, 'ascii')
+            if message == END_GAME_MESSAGE:
+                print(f"Player #{self._player_id} ended game {self._game.id}")
+                self._game.end()
+                return
+
+            print(f"{self._game.id}#{self._player_id}: {message}")
+            self._game.sendall(data)
+
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+
+if __name__ == "__main__":
+    # Port 0 means to select an arbitrary unused port
+    HOST, PORT = "localhost", 9999
+
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    with server:
+        ip, port = server.server_address
+
+        # Start a thread with the server -- that thread will then start one
+        # more thread for each request
+        server_thread = threading.Thread(target=server.serve_forever)
+        # Exit the server thread when the main thread terminates
+        server_thread.daemon = True
+        server_thread.start()
+        print("Server loop running in thread:", server_thread.name)
+
+        SERVER_STOP_EVENT.wait()
+
+        print("Shutting down")
+        server.shutdown()
